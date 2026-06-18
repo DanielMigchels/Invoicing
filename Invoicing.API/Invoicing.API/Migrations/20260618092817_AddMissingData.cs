2cc27154a8aa896cf1c60ef57af365969f5f547e
Daniel Migchels <danielm12@hotmail.nl>
2026-06-18 11:28:47 +0200

    Add missing migration

---
+++ Invoicing.API/Invoicing.API/Migrations/20260618092817_AddMissingData.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Invoicing.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMissingData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoices_UserId",
+                table: "Invoices",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_UserId",
+                table: "Customers",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Companies_UserId",
+                table: "Companies",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Companies_AspNetUsers_UserId",
+                table: "Companies",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customers_AspNetUsers_UserId",
+                table: "Customers",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Invoices_AspNetUsers_UserId",
+                table: "Invoices",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Companies_AspNetUsers_UserId",
+                table: "Companies");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customers_AspNetUsers_UserId",
+                table: "Customers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Invoices_AspNetUsers_UserId",
+                table: "Invoices");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Invoices_UserId",
+                table: "Invoices");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Customers_UserId",
+                table: "Customers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Companies_UserId",
+                table: "Companies");
+        }
+    }
+}
